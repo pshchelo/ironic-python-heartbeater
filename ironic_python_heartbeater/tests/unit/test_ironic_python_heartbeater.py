b851f7ed12abaad2c583a2f5397e38006cf6f5a9
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2017-03-16 15:55:00 +0200

    Add first unit test

--- ironic_python_heartbeater/tests/unit/test_ironic_python_heartbeater.py
+++ ironic_python_heartbeater/tests/unit/test_ironic_python_heartbeater.py
@@ -1,6 +1,15 @@
 import unittest
 
+import mock
+
+from ironic_python_heartbeater import ironic_python_heartbeater as iph
+
 
 class IronicPythonHeartbeaterTestCase(unittest.TestCase):
-    def test_dummy(self):
-        self.assertTrue(True)
+
+    def test__parse_kernel_cmdline(self):
+        fake_kernel_opts = "spam=ham foo=bar"
+        expected_opts = {'spam': 'ham', 'foo': 'bar'}
+        with mock.patch.object(iph, 'open',
+                               new=mock.mock_open(read_data=fake_kernel_opts)):
+            self.assertEqual(expected_opts, iph._parse_kernel_cmdline())
